31676d759f9ea0c57cf35623fc510bcec5e8fac6
Dimitri Steffanoni <68699435+Dimitri-Steffanoni@users.noreply.github.com>
2024-05-11 13:25:56 +0200

    Added functions for human and computer choices

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("Let's fight")
+console.log("Let's fight");
+
+function getComputerChoice() {
+  let i = Math.floor(Math.random() * 3);
+
+  if (i == 0) {
+    return "rock";
+  } else if (i == 1) {
+    return "paper";
+  } else {
+    return "scrissors";
+  }
+}
+
+function getHumanChoice() {
+  let resp = prompt("Choose rock paper or scissors");
+  let human_answer = resp.toLowerCase();
+  while (
+    human_answer != "rock" &&
+    human_answer != "paper" &&
+    human_answer != "scissors"
+  ) {
+    human_answer = prompt("Choose rock paper or scissors");
+  }
+  return human_answer;
+}
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice==computerChoice){
+        return humanChoice + " vs " + computerChoice + " no one wins";
+    } else {
+        return "brrrrr";
+    }
+}
+
+console.log(playRound());
+
+let humanScore = 0;
+let computerScore = 0;
